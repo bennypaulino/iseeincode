246307e6a75dbaf221ae9b1f05eb1f21223656da
Benny Paulino <santiagopro71@gmail.com>
2016-03-03 00:41:17 +0000

    Adjust line 1 for contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
-class Contact < MailFor::Base
+class Contact < MailForm::Base
   attribute :name,      :validate => true
-  attribute :email,     :validate => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   :validate => true
   attribute :nickname,  :validate => true
 
@@ -15,3 +15,5 @@ end
 
 # the Regex for email validation gets messed up if it starts with a forward slash like so:
 # /\A([\w\.%\+\-]+)@([\w\-]+.\)+([\w]{2,})\z/i
+
+# /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
